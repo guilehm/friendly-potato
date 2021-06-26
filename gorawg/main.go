dfea790c5e57fbb5079d9cfeea9aae99fb51777c
guilherme <guile.hm@hotmail.com>
2021-06-26 15:49:12 -0300

    add search handler

--- gorawg/main.go
+++ gorawg/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"gorawg/services"
 	"net/http"
 	"os"
 	"time"
 )
+
+func search(w http.ResponseWriter, r *http.Request) {
+	rawg := services.RawgService()
+	resp, err := rawg.SearchGame("GTA")
+
+	if err != nil {
+		fmt.Printf("ERROR: %v", err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	jsonResponse, err := json.Marshal(resp)
+	if err != nil {
+		fmt.Printf("ERROR: %v", err)
+	}
+	w.Write(jsonResponse)
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
@@ -14,5 +31,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("hello from gorawg")
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/search", search)
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }

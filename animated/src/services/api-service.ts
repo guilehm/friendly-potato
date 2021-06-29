2894f1f1b0a38cfd0e9c867aa95f16b7edda8ebb
guilherme <guile.hm@hotmail.com>
2021-06-29 19:18:11 -0300

    create api service

---
+++ animated/src/services/api-service.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios'
+
+const API_URL = process.env.API_URL || 'http://localhost:8090'
+
+class ApiService {
+  baseUrl: string
+  client: AxiosInstance
+
+  constructor(baseUrl: string = API_URL) {
+    this.baseUrl = `${baseUrl}`
+    this.client = axios.create()
+  }
+
+  getGameList(): Promise<AxiosResponse> {
+    return this.client.get(`${this.baseUrl}/games/`)
+  }
+
+}
+
+
+export default ApiService

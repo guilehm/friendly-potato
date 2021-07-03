9fe76f2514c548403dfad6c7deffd9b43663542c
guilherme <guile.hm@hotmail.com>
2021-07-02 22:40:19 -0300

    create handle api errors util

--- goapi/utils/utils.go
+++ goapi/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -10,4 +11,16 @@ func LogRequest(handler http.Handler) http.Handler {
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 		handler.ServeHTTP(w, r)
 	})
+}
+
+func HandleApiErrors(w http.ResponseWriter, status int, message string) {
+	if message == "" {
+		message = http.StatusText(status)
+	}
+
+	jsonResponse, _ := json.Marshal(struct {
+		Error string `json:"error"`
+	}{message})
+	w.WriteHeader(status)
+	w.Write(jsonResponse)
 }

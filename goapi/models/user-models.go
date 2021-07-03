dc240a64061d53cd88217fecf70b90c3d8e81d4f
guilherme <guile.hm@hotmail.com>
2021-07-02 22:40:27 -0300

    add user id info

--- goapi/models/user-models.go
+++ goapi/models/user-models.go
@@ -8,6 +8,7 @@ import (
 
 type User struct {
 	ID           primitive.ObjectID `bson:"_id"`
+	UserId       string             `json:"id"`
 	Email        *string            `json:"email" validate:"email,required"`
 	Password     *string            `json:"password" validate:"required"`
 	Token        *string            `json:"token"`

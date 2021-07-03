4242f212dc9d2470259f8455f1e0fb5543386998
guilherme <guile.hm@hotmail.com>
2021-07-02 22:53:07 -0300

    create generate tokens function

--- goapi/utils/utils.go
+++ goapi/utils/utils.go
@@ -4,7 +4,13 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
+
+var SECRET_KEY = os.Getenv("JWT_SECRET_KEY")
 
 func LogRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -23,4 +29,44 @@ func HandleApiErrors(w http.ResponseWriter, status int, message string) {
 	}{message})
 	w.WriteHeader(status)
 	w.Write(jsonResponse)
+}
+
+type SignedDetails struct {
+	Email string
+	Uid   string
+	jwt.StandardClaims
+}
+
+func GenerateTokens(email string, uid string) (signedToken string, signedRefreshToken string, err error) {
+	claims := &SignedDetails{
+		Email: email,
+		Uid:   uid,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Minute * time.Duration(10)).Unix(),
+		},
+	}
+
+	refreshClaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(
+		jwt.SigningMethodHS256, claims,
+	).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	refreshToken, err := jwt.NewWithClaims(
+		jwt.SigningMethodHS256, refreshClaims,
+	).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshToken, err
 }

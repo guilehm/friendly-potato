3f5f7895e631bf21bd4be88ed29a190f33c2c4d0
guilherme <guile.hm@hotmail.com>
2021-07-03 15:35:35 -0300

    update user struct for bson

--- goapi/models/user-models.go
+++ goapi/models/user-models.go
@@ -8,11 +8,11 @@ import (
 
 type User struct {
 	ID           primitive.ObjectID `bson:"_id"`
-	UserId       string             `json:"id"`
-	Email        *string            `json:"email" validate:"email,required"`
-	Password     *string            `json:"password" validate:"required"`
-	Token        *string            `json:"token"`
-	RefreshToken *string            `json:"refresh_token"`
-	DateAdded    time.Time          `json:"date_added"`
-	DateChanged  time.Time          `json:"date_changed"`
+	UserId       string             `bson:"id" json:"id"`
+	Email        *string            `bson:"email" json:"email" validate:"email,required"`
+	Password     *string            `bson:"password" json:"password" validate:"required"`
+	Token        *string            `bson:"token" json:"token"`
+	RefreshToken *string            `bson:"refresh_token" json:"refresh_token"`
+	DateAdded    time.Time          `bson:"date_added" json:"date_added"`
+	DateChanged  time.Time          `bson:"date_changed" json:"date_changed"`
 }

2e23c53180e7d3571af373443d88aae4a7369ba5
guilherme <guile.hm@hotmail.com>
2021-07-04 08:51:53 -0300

    create reset css style

--- animated/src/App.tsx
+++ animated/src/App.tsx
@@ -1,9 +1,19 @@
 import Layout from './templates/Layout'
 import Home from './templates/Home'
+import { createGlobalStyle } from 'styled-components'
 
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`
 
 const App = (): JSX.Element => (
   <div className="App">
+    <GlobalStyle />
     <Layout>
       <Home />
     </Layout>

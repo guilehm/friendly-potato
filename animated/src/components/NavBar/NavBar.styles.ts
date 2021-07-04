af249d6582b4524e29b5c0da2001ead18b548626
guilherme <guile.hm@hotmail.com>
2021-07-04 10:42:28 -0300

    add search button

--- animated/src/components/NavBar/NavBar.styles.ts
+++ animated/src/components/NavBar/NavBar.styles.ts
@@ -1,4 +1,24 @@
 import styled from 'styled-components'
 
+
+export const Nav = styled.nav`
+`
+
+export const SearchButton = styled.button`
+  background: url('/img/icons/search-icon.svg') no-repeat 95% 50%;
+  border: 0;
+  background-size: auto;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  cursor: pointer;
+`
+
 export const Container = styled.div`
+  max-width: 980px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 6px auto;
+  height: 40px;
 `
--- animated/src/components/NavBar/NavBar.tsx
+++ animated/src/components/NavBar/NavBar.tsx
@@ -3,9 +3,12 @@ import * as S from './NavBar.styles'
 
 
 const NavBar = (): JSX.Element => (
-  <S.Container>
-    <Avatar />
-  </S.Container>
+  <S.Nav>
+    <S.Container>
+      <S.SearchButton />
+      <Avatar />
+    </S.Container>
+  </S.Nav>
 )
 
 export default NavBar

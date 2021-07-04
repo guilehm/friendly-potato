1deade103ae2bb6a6227f344cd1f4aa490b4a56f
Guilherme <guile.hm@hotmail.com>
2021-07-04 11:27:25 -0300

    add link to login page

--- animated/src/components/NavBar/NavBar.tsx
+++ animated/src/components/NavBar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Avatar from '../Avatar'
 import * as S from './NavBar.styles'
 
@@ -5,7 +6,9 @@ import * as S from './NavBar.styles'
 const NavBar = (): JSX.Element => (
   <S.Nav>
     <S.Container>
-      <Avatar />
+      <Link to="/login">
+        <Avatar />
+      </Link>
     </S.Container>
   </S.Nav>
 )

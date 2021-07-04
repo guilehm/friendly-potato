8442f66fe195f166900f1131ce821212e33d38aa
guilherme <guile.hm@hotmail.com>
2021-07-04 09:39:32 -0300

    create navbar files

---
+++ animated/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,11 @@
+import Avatar from '../Avatar'
+import * as S from './NavBar.styles'
+
+
+const NavBar = (): JSX.Element => (
+  <S.Container>
+    <Avatar />
+  </S.Container>
+)
+
+export default NavBar

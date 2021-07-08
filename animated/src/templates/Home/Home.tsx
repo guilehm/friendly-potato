fff28a679cd61be4dd14a10caee873066ea80d72
guilherme <guile.hm@hotmail.com>
2021-07-08 19:34:13 -0300

    add loading spinner to home

--- animated/src/templates/Home/Home.tsx
+++ animated/src/templates/Home/Home.tsx
@@ -2,6 +2,7 @@ import { AxiosError, AxiosResponse } from 'axios'
 import { useEffect, useState } from 'react'
 import Card from '../../components/Card/Card'
 import NavBar from '../../components/NavBar'
+import Spinner from '../../components/Spinner'
 import ApiService from '../../services/api-service'
 import * as S from './Home.styles'
 
@@ -16,14 +17,17 @@ type GameListResult = {
 const Api = new ApiService()
 
 const Home = (): JSX.Element => {
+  const [loading, setLoading] = useState(true)
   const [gameList, setGameList] = useState<GameListResult[]>([])
 
   useEffect(() => {
     const fetchGameList = async () => {
 
       const handleError = (error: AxiosError) => console.log(error)
-      const handleSuccess = (response: AxiosResponse) =>
+      const handleSuccess = (response: AxiosResponse) => {
         setGameList(response.data.results)
+        setLoading(false)
+      }
 
       Api.getGameList()
         .then(handleSuccess)
@@ -35,11 +39,13 @@ const Home = (): JSX.Element => {
   return (
     <S.HomeContainer>
       <S.HomeSection>
-        {gameList.map(game =>
-          <Card
-            key={game.slug}
-            title={game.name}
-            image={game.background_image} />)}
+        {loading ? <Spinner /> :
+          gameList.map(game =>
+            <Card
+              key={game.slug}
+              title={game.name}
+              image={game.background_image} />)
+        }
       </S.HomeSection>
     </S.HomeContainer>
   )

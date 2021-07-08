bf32a84f17c83f620447e236442d0687b17c7b19
guilherme <guile.hm@hotmail.com>
2021-07-08 19:34:02 -0300

    Create spinner component

---
+++ animated/src/components/Spinner/Spinner.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { Spinner as ChakraSpinner } from '@chakra-ui/react'
+
+
+export const Spinner = styled(ChakraSpinner)`
+  position: fixed;
+  top: 50%;
+  right: 50%;
+`
---
+++ animated/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,6 @@
+import * as S from './Spinner.styles'
+
+const Spinner = (): JSX.Element => <S.Spinner />
+
+
+export default Spinner

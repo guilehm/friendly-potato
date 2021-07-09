1bc0416703ba1dcd4705d05da54851ecfd601b16
guilherme <guile.hm@hotmail.com>
2021-07-09 19:46:07 -0300

    simplify password input

--- animated/src/templates/Login/Login.tsx
+++ animated/src/templates/Login/Login.tsx
@@ -37,29 +37,6 @@ const Login = (): JSX.Element => {
 
   const onSubmit = (values: LoginFormInputs) => console.log('****', values)
 
-  function PasswordInput() {
-    const [show, setShow] = useState(false)
-    const handleClick = () => setShow(!show)
-
-    return (
-      <InputGroup size="md">
-        <Input
-          pr="4.5rem"
-          type={show ? 'text' : 'password'}
-          placeholder="password"
-          id="password"
-          isInvalid={!!errors.password}
-          {...register('password')}
-        />
-        <InputRightElement width="4.5rem">
-          <Button h="1.75rem" size="sm" onClick={handleClick}>
-            {show ? 'Hide' : 'Show'}
-          </Button>
-        </InputRightElement>
-      </InputGroup>
-    )
-  }
-
   return (
     <S.Container maxW="xl" centerContent>
       <S.Section>
@@ -81,11 +58,16 @@ const Login = (): JSX.Element => {
                 <FormHelperText display="none">{'We\'ll never share your email.'}</FormHelperText>
 
                 <FormLabel display="none">Password</FormLabel>
-                <PasswordInput />
+                <Input
+                  placeholder="password"
+                  type="password"
+                  {...register('password')}
+                  isInvalid={!!errors.password}
+                />
                 {errors.password && <FormErrorMessage>{errors.password.message}</FormErrorMessage>}
 
                 <HStack>
-                  {/* <Button onClick={handleSubmit(onSubmit)} size="sm">Register</Button> */}
+                  <Button onClick={handleSubmit(onSubmit)} size="sm">Register</Button>
                   <Button type="submit" size="sm">Sign In</Button>
                 </HStack>
 

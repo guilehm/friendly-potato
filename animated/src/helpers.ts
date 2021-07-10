42cf2e4a3838fa960e399039e062d218c215be3d
guilherme <guile.hm@hotmail.com>
2021-07-10 07:52:22 -0300

    create toast data helper

---
+++ animated/src/helpers.ts
@@ -0,0 +1,20 @@
+type ToastData = {
+  title: string,
+  description?: string,
+  status?: 'info' | 'warning' | 'success' | 'error',
+  duration?: number,
+  isClosable?: boolean,
+}
+
+const makeToastData = ({
+  title,
+  description = '',
+  status = 'success',
+  duration = 5000,
+  isClosable = true,
+}: ToastData): ToastData => ({
+  title: title, description, status, duration, isClosable,
+})
+
+
+export { makeToastData }

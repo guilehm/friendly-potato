efccd12bb14286365da486f5e505fd854ec2ffd3
guilherme <guile.hm@hotmail.com>
2021-07-10 12:28:44 -0300

    update cors settings

--- goapi/main.go
+++ goapi/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"goapi/handlers"
+	"goapi/middlewares"
 	"goapi/utils"
 	"net/http"
 	"os"
@@ -15,8 +16,13 @@ func main() {
 	fmt.Println("hello from goapi")
 
 	r := mux.NewRouter()
-	handler := cors.Default().Handler(r)
-	r.StrictSlash(true).HandleFunc("/games/", handlers.GameList)
+	handler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodOptions},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+	}).Handler(r)
+	r.StrictSlash(true).HandleFunc("/games/", middlewares.Authentication(handlers.GameList))
 	r.StrictSlash(true).HandleFunc("/games/{id}/", handlers.GameDetail)
 	r.StrictSlash(true).HandleFunc("/users/", handlers.SignUp).Methods("POST")
 	r.StrictSlash(true).HandleFunc("/users/login/", handlers.Login).Methods("POST")

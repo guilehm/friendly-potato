27d95aa36761d96cd7cc7ce346f2316cc8f0a726
guilherme <guile.hm@hotmail.com>
2021-07-10 12:25:39 -0300

    create request interceptor

--- animated/src/services/api-service.ts
+++ animated/src/services/api-service.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios'
+import { Cookies } from 'react-cookie'
 
 const API_URL = process.env.API_URL || 'http://localhost:8080'
 
@@ -14,7 +15,26 @@ class ApiService {
 
   constructor(baseUrl: string = API_URL) {
     this.baseUrl = `${baseUrl}`
-    this.client = axios.create()
+    this.client = this.getClient()
+  }
+
+  getClient(): AxiosInstance {
+    const cookies = new Cookies(['access'])
+
+    const client = axios.create()
+    client.interceptors.request.use(
+      config => {
+        const accessToken = cookies.get('access')
+        if (accessToken) {
+          config.headers['Authorization'] = accessToken
+        }
+        return config
+      },
+      error => {
+        Promise.reject(error)
+      }
+    )
+    return client
   }
 
   getGameList(): Promise<AxiosResponse> {

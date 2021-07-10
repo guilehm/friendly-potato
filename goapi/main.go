7ef15e8110eec842ce64e722f61f98674769eb9a
guilherme <guile.hm@hotmail.com>
2021-07-09 21:17:24 -0300

    update handler

--- goapi/main.go
+++ goapi/main.go
@@ -8,15 +8,17 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
 	fmt.Println("hello from goapi")
 
 	r := mux.NewRouter()
+	handler := cors.Default().Handler(r)
 	r.StrictSlash(true).HandleFunc("/games/", handlers.GameList)
 	r.StrictSlash(true).HandleFunc("/games/{id}/", handlers.GameDetail)
 	r.StrictSlash(true).HandleFunc("/users/", handlers.SignUp).Methods("POST")
 	r.StrictSlash(true).HandleFunc("/users/login/", handlers.Login).Methods("POST")
-	http.ListenAndServe(":"+os.Getenv("PORT"), utils.LogRequest(utils.SetHeaders(r)))
+	http.ListenAndServe(":"+os.Getenv("PORT"), utils.SetHeaders(utils.LogRequest(handler)))
 }

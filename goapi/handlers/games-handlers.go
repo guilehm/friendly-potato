5ac9fbe234c1bd670b953f5b98d948fe385688c1
guilherme <guile.hm@hotmail.com>
2021-07-11 09:41:46 -0300

    improve error handling

--- goapi/handlers/games-handlers.go
+++ goapi/handlers/games-handlers.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"goapi/db"
 	"goapi/services"
+	"goapi/utils"
 	"net/http"
 	"strconv"
 
@@ -16,11 +18,11 @@ func GameList(w http.ResponseWriter, r *http.Request) {
 	resp, err := rawg.SearchGame(r.URL.Query())
 
 	if err != nil {
-		fmt.Printf("ERROR: %v", err)
+		fmt.Printf("ERROR: %v\n", err)
 	}
 	jsonResponse, err := json.Marshal(resp)
 	if err != nil {
-		fmt.Printf("ERROR: %v", err)
+		fmt.Printf("ERROR: %v\n", err)
 	}
 
 	db.OpenCollection("game-list")
@@ -35,17 +37,24 @@ func GameDetail(w http.ResponseWriter, r *http.Request) {
 	rawg := services.RawgService()
 	intId, err := strconv.Atoi(id)
 	if err != nil {
-		fmt.Printf("Error: invalid ID")
+		fmt.Println("Error: invalid ID")
+		utils.HandleApiErrors(w, http.StatusBadRequest, "invalid ID")
 	}
 
 	resp, err := rawg.GetGameDetail(intId)
 	if err != nil {
-		fmt.Printf("ERROR: %v", err)
+		if errors.Is(err, services.ErrNotFound) {
+			utils.HandleApiErrors(w, http.StatusNotFound, "")
+			return
+		}
+		fmt.Printf("ERROR: %v\n", err)
+		utils.HandleApiErrors(w, http.StatusInternalServerError, "")
+		return
 	}
 
 	jsonResponse, err := json.Marshal(resp)
 	if err != nil {
-		fmt.Printf("ERROR: %v", err)
+		fmt.Printf("ERROR: %v\n", err)
 	}
 
 	w.Write(jsonResponse)

3e936e04ac61a459cdfc2690574d7a58109ad97d
guilherme <guile.hm@hotmail.com>
2021-07-20 10:32:29 -0300

    create request middlewares

---
+++ goapi/middlewares/request-middlewares.go
@@ -0,0 +1,20 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+)
+
+func SetHeaders(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func LogRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}

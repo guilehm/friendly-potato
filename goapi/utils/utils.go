12bba36e28c4a149ebed13778cd5e318ee4e5879
guilherme <guile.hm@hotmail.com>
2021-07-20 10:32:45 -0300

    move request middlewares from utils to middlewares package

--- goapi/utils/utils.go
+++ goapi/utils/utils.go
@@ -2,26 +2,11 @@ package utils
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"os"
 )
 
 var SECRET_KEY = os.Getenv("JWT_SECRET_KEY")
-
-func SetHeaders(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		handler.ServeHTTP(w, r)
-	})
-}
-
-func LogRequest(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-		handler.ServeHTTP(w, r)
-	})
-}
 
 func HandleApiErrors(w http.ResponseWriter, status int, message string) {
 	if message == "" {

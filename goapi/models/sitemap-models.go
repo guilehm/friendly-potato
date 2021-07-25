8f84a50cf95a212efabf74b3bc149115a2f6c7b7
Guilherme <guile.hm@hotmail.com>
2021-07-25 18:18:43 -0300

    update models for bson

--- goapi/models/sitemap-models.go
+++ goapi/models/sitemap-models.go
@@ -3,11 +3,11 @@ package models
 import "encoding/xml"
 
 type Sitemap struct {
-	Location string `xml:"loc"`
-	LastMod  string `xml:"lastmod"`
+	Location string `xml:"loc" bson:"location"`
+	LastMod  string `xml:"lastmod" bson:"lastmod"`
 }
 
 type SitemapIndex struct {
-	XMLName  xml.Name  `xml:"sitemapindex"`
-	Sitemaps []Sitemap `xml:"sitemap"`
+	XMLName  xml.Name  `xml:"sitemapindex" bson:"sitemapindex"`
+	Sitemaps []Sitemap `xml:"sitemap" bson:"sitemap"`
 }

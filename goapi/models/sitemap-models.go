c29b365164342b972213fa4e99567cf02dc17b7b
Guilherme <guile.hm@hotmail.com>
2021-07-25 23:19:48 -0300

    improve sitemap models

--- goapi/models/sitemap-models.go
+++ goapi/models/sitemap-models.go
@@ -7,9 +7,15 @@ type Sitemap struct {
 	LastMod    string `xml:"lastmod" bson:"lastmod"`
 	ChangeFreq string `xml:"changefreq" bson:"changefreq,omitempty"`
 	Priority   string `xml:"priority" bson:"priority,omitempty"`
+	Crawled    bool   `xml:"-" bson:"crawled"`
 }
 
 type SitemapIndex struct {
 	XMLName  xml.Name  `xml:"sitemapindex" bson:"sitemapindex"`
 	Sitemaps []Sitemap `xml:"sitemap" bson:"sitemap"`
+}
+
+type SitemapDetail struct {
+	XMLName  xml.Name  `xml:"urlset" bson:"urlset"`
+	Sitemaps []Sitemap `xml:"url" bson:"url"`
 }

cca069f90da9ed18d1da4ca76b453baaa57d4459
guilherme <guile.hm@hotmail.com>
2021-12-28 20:12:46 -0300

    lint fixes

--- goapi/models/lumber-models.go
+++ goapi/models/lumber-models.go
@@ -1,23 +1,24 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type Wood struct {
-	Name string
-	Color string
+	Name        string
+	Color       string
 	DateCreated time.Time
 }
 
 type WoodPile struct {
-	Woods []Wood
+	Woods       []Wood
 	DateCreated time.Time
 }
 
 type Player struct {
-	ID string
-	Username string
-	Coins int
-	Sprite string
+	Username  string
+	Coins     int
+	Sprite    string
 	LastLogin time.Time
-	WoodPile WoodPile
+	WoodPile  WoodPile
 }

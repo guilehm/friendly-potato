acbd93fd96cd5e21e324ee6d88c0fe079c73f260
guilherme <guile.hm@hotmail.com>
2021-12-28 20:49:54 -0300

    create lumber component

---
+++ animated/src/templates/Lumber/Lumber.styles.ts
@@ -0,0 +1,6 @@
+
+import styled from "styled-components"
+
+export const LumberContainer = styled.div`
+  padding-bottom: 20px;
+`
---
+++ animated/src/templates/Lumber/Lumber.tsx
@@ -0,0 +1,14 @@
+import * as S from "./Lumber.styles"
+
+
+const Lumber = (): JSX.Element => {
+
+  return (
+    <S.LumberContainer>
+      <h1>Hello from Lumber</h1>
+    </S.LumberContainer>
+  )
+}
+
+
+export default Lumber

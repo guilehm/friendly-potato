1760cf0a2e08440e68b49d437f3715118ac8151a
guilherme <guile.hm@hotmail.com>
2021-12-29 11:00:08 -0300

    create lumber styles

--- animated/src/templates/Lumber/Lumber.styles.ts
+++ animated/src/templates/Lumber/Lumber.styles.ts
@@ -4,3 +4,18 @@ import styled from "styled-components"
 export const LumberContainer = styled.div`
   padding-bottom: 20px;
 `
+
+export const Image = styled.img`
+  width: 30px;
+  height: 30px;
+`
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: row;
+`
+
+export const ListItem = styled.li`
+  display: flex;
+  padding: 20px;
+`

31c5d5e5b94ac218f211a0ab02da4ac6c2b0ec84
guilherme <guile.hm@hotmail.com>
2021-12-30 11:12:01 -0300

    create util to get player data

--- goapi/utils/lumber-util.go
+++ goapi/utils/lumber-util.go
@@ -2,9 +2,13 @@ package utils
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"goapi/db"
 	"goapi/models"
 	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -29,4 +33,33 @@ func UpdatePlayerData(playerData *models.PlayerData, user models.User) error {
 		&opt,
 	)
 	return err
+}
+
+func GetPlayerData(user models.User) (models.PlayerData, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	playerData := models.PlayerData{}
+	err := lumberCollection.FindOne(ctx, bson.M{"user_id": user.UserId}).Decode(&playerData)
+	if err != nil {
+		if !errors.Is(err, mongo.ErrNoDocuments) {
+			fmt.Println("Error while trying to find player:", err)
+			return playerData, err
+		} else {
+			playerData = models.PlayerData{
+				UserId:    user.UserId,
+				Gold:      0,
+				Sprite:    "",
+				LastLogin: time.Now().Local(),
+				Woods:     &[]models.Wood{},
+			}
+			_, err := lumberCollection.InsertOne(ctx, playerData)
+			if err != nil {
+				fmt.Println("Could not create user data:", err)
+				return playerData, err
+			}
+			fmt.Println("Player data created for", *user.Email)
+		}
+	}
+	return playerData, err
 }

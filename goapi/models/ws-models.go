925c503f7567d2e88192b70e6bfa2d12e634d5e5
guilherme <guile.hm@hotmail.com>
2022-01-01 09:54:45 -0300

    create hub start function

--- goapi/models/ws-models.go
+++ goapi/models/ws-models.go
@@ -20,8 +20,31 @@ type Client struct {
 }
 
 type Hub struct {
-	clients    map[*Client]bool
-	broadcast  chan []byte
-	register   chan *Client
-	unregister chan *Client
+	Clients    map[*Client]bool
+	Broadcast  chan []byte
+	Register   chan *Client
+	Unregister chan *Client
+}
+
+func (h *Hub) Start() {
+	for {
+		select {
+		case client := <-h.Register:
+			h.Clients[client] = true
+		case client := <-h.Unregister:
+			if _, ok := h.Clients[client]; ok {
+				delete(h.Clients, client)
+				close(client.send)
+			}
+		case message := <-h.Broadcast:
+			for client := range h.Clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.Clients, client)
+				}
+			}
+		}
+	}
 }

4d6c8a24cdccbd2339272503b986da6a2fac3771
guilherme <guile.hm@hotmail.com>
2022-01-01 00:18:36 -0300

    create socket connection

--- animated/src/templates/RPG/RPG.tsx
+++ animated/src/templates/RPG/RPG.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react"
+import { WSMessage } from "../../types/ws-types"
 import * as S from "./RPG.styles"
 
 const CHARACTER_SIZE = 100
@@ -9,6 +10,30 @@ const CANVAS_HEIGHT = 800
 const RPG = (): JSX.Element => {
 
   const canvasRef = useRef<HTMLCanvasElement>()
+
+  useEffect(() => {
+    // TODO: remove hardcoded url
+    const location = "ws://localhost:8080/ws/rpg/"
+    const webSocket = new WebSocket(location)
+
+    const message: WSMessage = {
+      type: "game-join",
+      data: { "username": "guilehm" },
+    }
+
+    webSocket.onopen = () => {
+      webSocket.send(JSON.stringify(message))
+    }
+
+    webSocket.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      if (data.type === "login-successful") {
+        console.log(event.data)
+      }
+    }
+
+
+  }, [])
 
   useEffect(() => {
 

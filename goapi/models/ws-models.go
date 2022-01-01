50bd546e9d8f1f91d65aab7c43ecb56153be57a9
guilherme <guile.hm@hotmail.com>
2022-01-01 01:33:44 -0300

    create client and hub structs

--- goapi/models/ws-models.go
+++ goapi/models/ws-models.go
@@ -1,10 +1,27 @@
 package models
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"github.com/gorilla/websocket"
+)
 
 type WSMessageType string
 
 type WSMessage struct {
 	MessageType WSMessageType   `json:"type"`
 	Data        json.RawMessage `json:"data"`
+}
+
+type Client struct {
+	hub  *Hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
 }

d715a8bf3c3966fbb0d4075d570c75a33b35548a
guilherme <guile.hm@hotmail.com>
2022-01-01 17:23:45 -0300

    create character sprite map

---
+++ animated/src/constants.ts
@@ -0,0 +1,10 @@
+export const CharacterSpriteMap = new Map([
+  ["human", "/img/assets/characters/human.png"],
+  ["orc", "/img/assets/characters/orc.png"],
+  ["skeleton", "/img/assets/characters/skeleton.png"],
+  ["archer", "/img/assets/characters/archer.png"],
+  ["vampire", "/img/assets/characters/vampire.png"],
+  ["berzerker", "/img/assets/characters/berzerker.png"],
+])
+
+

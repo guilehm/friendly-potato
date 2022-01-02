9ecc79be1e8056a189229de2d7a5956c3e61432a
guilherme <guile.hm@hotmail.com>
2022-01-02 16:39:55 -0300

    create constants file

---
+++ goapi/constants/rpg-constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const WalkStep = 10
+const borderOffset = 10
+const CharacterSize = 50
+const MaxPosX = 1000/2 - CharacterSize - borderOffset
+const MaxPosY = 800/2 - CharacterSize - borderOffset
+const MinPosX = 0 + borderOffset
+const MinPosY = 20 + borderOffset

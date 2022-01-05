f043943adc5ffa738622462f8316c5d856ef40d4
guilherme <guile.hm@hotmail.com>
2022-01-04 23:30:05 -0300

    create has collision

--- goapi/models/rpg-models.go
+++ goapi/models/rpg-models.go
@@ -62,6 +62,29 @@ type RPGBroadcast struct {
 	PlayerCount int           `json:"player_count"`
 }
 
+func HasCollision(startXp1, startYp1, startXp2, startYp2, offset int) (bool, bool) {
+	collisionX := false
+	collisionY := false
+
+	startXp1 -= offset
+	startYp1 -= offset
+	startXp2 -= offset
+	startYp2 -= offset
+
+	endXp1 := startXp1 + constants.CharacterSize + offset
+	endYp1 := startYp1 + constants.CharacterSize + offset
+	endXp2 := startXp2 + constants.CharacterSize + offset
+	endYp2 := startYp2 + constants.CharacterSize + offset
+
+	if startXp2 < endXp1 && endXp2 > startXp1 {
+		collisionY = true
+	}
+	if startYp2 < endYp1 && endYp2 > startYp1 {
+		collisionX = true
+	}
+	return collisionX, collisionY
+}
+
 func (p *Player) GetCollisions(player2 Player, offset int) (bool, bool) {
 	collisionX := false
 	collisionY := false

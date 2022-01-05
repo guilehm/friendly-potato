c21d37a33297c7edba33ac5bb916ab9db1e63a41
guilherme <guile.hm@hotmail.com>
2022-01-05 20:29:15 -0300

    remove game container

--- animated/src/components/RPG/RPG.styles.ts
+++ animated/src/components/RPG/RPG.styles.ts
@@ -3,15 +3,6 @@ import { Container as ChakraContainer } from "@chakra-ui/react"
 
 export const Container = styled(ChakraContainer)``
 
-export const GameContainer = styled.div`
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  /* z-index: 10; */
-`
-
 export const Canvas = styled.canvas`
   border: 1px solid black;
   box-shadow: 0px 0px 10px -3px #000000;

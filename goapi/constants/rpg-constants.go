437f26aa2ca9cafb83ae873655b0be036a29b167
guilherme <guile.hm@hotmail.com>
2022-01-05 21:50:20 -0300

    reorder constants for a better readability

--- goapi/constants/rpg-constants.go
+++ goapi/constants/rpg-constants.go
@@ -1,8 +1,8 @@
 package constants
 
+const CharacterSize = 50
 const WalkStep = 10
 const borderOffset = 10
-const CharacterSize = 50
 const MaxPosX = 1000/2 - CharacterSize - borderOffset
 const MaxPosY = 800/2 - CharacterSize - borderOffset
 const MinPosX = 0 + borderOffset

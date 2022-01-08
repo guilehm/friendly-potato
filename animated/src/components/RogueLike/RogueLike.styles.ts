81e765883cde6f3b2e12d714905bbb38801efec1
guilherme <guile.hm@hotmail.com>
2022-01-08 11:03:28 -0300

    improve canvas css

--- animated/src/components/RogueLike/RogueLike.styles.ts
+++ animated/src/components/RogueLike/RogueLike.styles.ts
@@ -4,6 +4,7 @@ export const Container = styled.div`
 `
 
 export const Canvas = styled.canvas`
-  border: 10px solid black;
+  border: 5px solid black;
+  box-shadow: 0px 0px 20px -3px #000000;
   margin: auto;
 `

46da4d073fc1fb933e9325a5b8ad2286cd81718b
guilherme <guile.hm@hotmail.com>
2022-01-08 11:03:40 -0300

    create sprite and player types

--- animated/src/types/rogue-types.ts
+++ animated/src/types/rogue-types.ts
@@ -1,3 +1,27 @@
 type CharacterType = string
+type TileSet = string
 
 export const Warrior: CharacterType = "warrior"
+export const Background: TileSet = "background"
+export const Characters: TileSet = "characters"
+
+
+type Sprite = {
+  name: CharacterType
+  attackRange: number
+  hp: number
+  moveRange: number
+  spriteX: 0
+  spriteY: 0
+  spriteHeight: number
+  spriteWidth: number
+  tileSet: TileSet
+}
+
+export type Player = {
+  health: number,
+  positionX: number
+  positionY: number
+  sprite: Sprite
+}
+

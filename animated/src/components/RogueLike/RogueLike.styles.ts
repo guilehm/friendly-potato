1c79567d11d23f8f6db91831f59ed5d5bad896b8
guilherme <guile.hm@hotmail.com>
2022-01-07 23:00:00 -0300

    create rogue like component

---
+++ animated/src/components/RogueLike/RogueLike.styles.ts
@@ -0,0 +1,4 @@
+import styled from "styled-components"
+import { Container as ChakraContainer } from "@chakra-ui/react"
+
+export const Container = styled(ChakraContainer)``
---
+++ animated/src/components/RogueLike/RogueLike.tsx
@@ -0,0 +1,11 @@
+import * as S from "./RogueLike.styles"
+
+
+const RogueLike = (): JSX.Element => {
+  return (
+    <S.Container>hello from rogue like</S.Container>
+  )
+}
+
+
+export default RogueLike

5a0f779a6db17cbc1aa688387baa0a29802f5b87
guilherme <guile.hm@hotmail.com>
2022-01-08 11:47:11 -0300

    create onStart function

--- animated/src/components/RogueLike/RogueLike.tsx
+++ animated/src/components/RogueLike/RogueLike.tsx
@@ -5,29 +5,58 @@ import * as S from "./RogueLike.styles"
 
 const RogueLike = (): JSX.Element => {
 
+  const [start, setStart] = useState(false)
   const [ws, setWs] = useState<WebSocket | null>(null)
   const canvasRef = useRef() as MutableRefObject<HTMLCanvasElement>
 
-  function drawBackground(
+  let PLAYERS_DATA: Array<Player> = []
+
+  const connect = () => {
+    const location = process.env.REACT_APP_ROGUE_WS_LOCATION || "ws://localhost:8080/ws/rogue/"
+    const webSocket = new WebSocket(location)
+    setWs(webSocket)
+
+    webSocket.onopen = () => {
+      webSocket.send(JSON.stringify({
+        type: "user-joins",
+        data: { "sprite": Warrior },
+      }))
+    }
+
+    webSocket.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      if (data.type === "broadcast") {
+        PLAYERS_DATA = data.players
+      }
+    }
+    animate()
+  }
+
+  const onStart = () => {
+    setStart(true)
+    connect()
+  }
+
+  const drawBackground = (
     canvas: HTMLCanvasElement,
     ctx: CanvasRenderingContext2D,
     dx: number,
     dy: number,
     dw: number,
     dh: number,
-  ) {
+  ) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     const background = new Image()
     background.src = `${window.location.origin}/img/assets/rogue/sprites/background.png`
     ctx.drawImage(background, dx, dy, dw, dh)
   }
 
-  function drawPlayer(
+  const drawPlayer = (
     ctx: CanvasRenderingContext2D,
     player: Player,
-  ) {
+  ) => {
     const image = new Image()
-    image.src = `${window.location.origin}/img/assets/rogue/sprites/${player.sprite.name}.png`
+    image.src = `${window.location.origin}/img/assets/rogue/sprites/${player.sprite.tileSet}.png`
     const sprite = player.sprite
     ctx.drawImage(
       image, sprite.spriteX, sprite.spriteY, sprite.spriteWidth, sprite.spriteHeight

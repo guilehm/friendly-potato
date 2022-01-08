dd5bb5537ccc6d2dc2fc6d52edf60c3ef9443cb7
guilherme <guile.hm@hotmail.com>
2022-01-08 11:46:52 -0300

    css updates

--- animated/src/components/RogueLike/RogueLike.styles.ts
+++ animated/src/components/RogueLike/RogueLike.styles.ts
@@ -1,10 +1,21 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
 `
 
 export const Canvas = styled.canvas`
   border: 5px solid black;
   box-shadow: 0px 0px 20px -3px #000000;
   margin: auto;
+  width: 50%;
+  image-rendering: optimizeSpeed; /* STOP SMOOTHING, GIVE ME SPEED  */
+  image-rendering: -moz-crisp-edges; /* Firefox                        */
+  image-rendering: -o-crisp-edges; /* Opera                          */
+  image-rendering: -webkit-optimize-contrast; /* Chrome (and eventually Safari) */
+  image-rendering: pixelated; /* Chrome */
+  image-rendering: optimize-contrast; /* CSS3 Proposed                  */
+  -ms-interpolation-mode: nearest-neighbor;
 `

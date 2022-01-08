21906b1780c6d19ce02e297b66331074a817ca37
guilherme <guile.hm@hotmail.com>
2022-01-08 10:52:32 -0300

    create animate and set background functions

--- animated/src/components/RogueLike/RogueLike.tsx
+++ animated/src/components/RogueLike/RogueLike.tsx
@@ -8,6 +8,36 @@ const RogueLike = (): JSX.Element => {
   const [ws, setWs] = useState<WebSocket | null>(null)
   const canvasRef = useRef() as MutableRefObject<HTMLCanvasElement>
 
+  function drawBackground(
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+    dx: number,
+    dy: number,
+    dw: number,
+    dh: number,
+  ) {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    const background = new Image()
+    background.src = `${window.location.origin}/img/assets/rogue/sprites/background.png`
+    ctx.drawImage(background, dx, dy, dw, dh)
+
+  }
+
+  const animate = useCallback(() => {
+    const canvas = canvasRef.current
+    if (!canvas) return
+    const ctx = canvas.getContext("2d")
+    if (!ctx) return
+
+    requestAnimationFrame(animate)
+
+    const dx = 0
+    const dy = 0
+    drawBackground(canvas, ctx, dx, dy, canvas.width, canvas.height)
+
+  }, [])
+
   useEffect(() => {
     const location = process.env.REACT_APP_ROGUE_WS_LOCATION || "ws://localhost:8080/ws/rogue/"
     const webSocket = new WebSocket(location)

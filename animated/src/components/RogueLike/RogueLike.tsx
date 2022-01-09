7971dff9097dad333af65b3244d97b2a58b8e69e
guilherme <guile.hm@hotmail.com>
2022-01-08 23:59:21 -0300

    create logic to animate

--- animated/src/components/RogueLike/RogueLike.tsx
+++ animated/src/components/RogueLike/RogueLike.tsx
@@ -1,6 +1,6 @@
 import { MutableRefObject, useRef, useState } from "react"
 import { ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT, ARROW_UP } from "../../constants"
-import { Player, Warrior } from "../../types/rogue-types"
+import { Player, Positions, Sprite, Warrior } from "../../types/rogue-types"
 import { WSMessage } from "../../types/ws-types"
 import * as S from "./RogueLike.styles"
 
@@ -50,18 +50,18 @@ const RogueLike = (): JSX.Element => {
   const drawPlayer = (
     ctx: CanvasRenderingContext2D,
     player: Player,
+    sprite: Positions,
   ) => {
     const image = new Image()
     image.src = `${window.location.origin}/img/assets/rogue/sprites/${player.sprite.tileSet}.png`
-    const sprite = player.sprite
     ctx.drawImage(
       image,
       sprite.spriteX,
       sprite.spriteY,
       sprite.spriteWidth,
       sprite.spriteHeight,
-      player.positionX + sprite.xOffset,
-      player.positionY + sprite.yOffset,
+      player.positionX + sprite.xOffset || 0,
+      player.positionY + sprite.yOffset || 0,
       sprite.spriteWidth,
       sprite.spriteHeight,
     )
@@ -80,7 +80,22 @@ const RogueLike = (): JSX.Element => {
     const dx = 0
     const dy = 0
     drawBackground(canvas, ctx, dx, dy, canvas.width, canvas.height)
-    PLAYERS_DATA.forEach((player) => drawPlayer(ctx, player))
+
+    const now = Date.now()
+    PLAYERS_DATA.forEach((player) => {
+      let sprite
+      if (player.lastAnimationTime === undefined) player.lastAnimationTime = now
+      if (player.animation) {
+        sprite = player.sprite.animation
+      } else {
+        sprite = player.sprite
+      }
+      if (now > (player.lastAnimationTime + player.sprite.animationPeriod)) {
+        player.animation = !player.animation
+        player.lastAnimationTime = now
+      }
+      drawPlayer(ctx, player, sprite)
+    })
 
   }
 

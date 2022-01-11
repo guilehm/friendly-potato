a8441cde971808e15af9c7fd5dc1ed9f7a84f8f3
guilherme <guile.hm@hotmail.com>
2022-01-11 18:31:47 -0300

    frontend does not need to handle player movements

--- animated/src/services/game-service.ts
+++ animated/src/services/game-service.ts
@@ -13,35 +13,5 @@ export const handleAnimations = (player: Player, now: number) => {
     player.animation = !player.animation
     player.lastAnimationTime = now
   }
-
-  if (
-    player.lastInteraction === true &&
-    (player.lastPosition.positionX !== player.positionX) ||
-    (player.lastPosition.positionY !== player.positionY)
-  ) {
-
-    if (!player.movingPosition) player.movingPosition = {
-      positionX: player.lastPosition.positionX,
-      positionY: player.lastPosition.positionY,
-    }
-
-    if (
-      (player.movingPosition.positionX !== player.positionX) ||
-      (player.movingPosition.positionY !== player.positionY)
-    ) {
-      if (!player.lastMovingTime) player.lastMovingTime = now
-      player.moving = true
-      if (now > (player.lastMovingTime + (player.sprite.animationPeriod / 40))) {
-        player.lastMovingTime = now
-
-        if (player.movingPosition.positionX < player.positionX) player.movingPosition.positionX += 1
-        if (player.movingPosition.positionX > player.positionX) player.movingPosition.positionX -= 1
-        if (player.movingPosition.positionY < player.positionY) player.movingPosition.positionY += 1
-        if (player.movingPosition.positionY > player.positionY) player.movingPosition.positionY -= 1
-      }
-    } else {
-      player.moving = false
-    }
-  }
   return sprite
 }

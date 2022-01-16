e993f730ffb7036ef2f57184b42672abf5b6596e
guilherme <guile.hm@hotmail.com>
2022-01-16 17:52:27 -0300

    update function to handle animations independent of backend responses

--- animated/src/services/game-service.ts
+++ animated/src/services/game-service.ts
@@ -1,17 +1,24 @@
 import { Player } from "../types/rogue-types"
 
-export const handleAnimations = (player: Player, now: number) => {
+interface AnimationTime {
+  [key: string]: [number, boolean]
+}
+const lastAnimationMap: AnimationTime = {}
 
+
+export const handleAnimations = (player: Player, now: number) => {
+  // TODO: improve performance
   let sprite
-  if (player.lastAnimationTime === undefined) player.lastAnimationTime = now
-  if (player.animation) {
+  const animationData = lastAnimationMap[player.id.toString()]
+  if (!animationData) lastAnimationMap[player.id.toString()] = [now, false]
+  const [lastAnimation, animated] = animationData
+  if (animated) {
     sprite = player.sprite.animation
   } else {
     sprite = player.sprite
   }
-  if (now > (player.lastAnimationTime + player.sprite.animationPeriod)) {
-    player.animation = !player.animation
-    player.lastAnimationTime = now
+  if (now > (lastAnimation + player.sprite.animationPeriod)) {
+    lastAnimationMap[player.id.toString()] = [now, !animated]
   }
   return sprite
 }

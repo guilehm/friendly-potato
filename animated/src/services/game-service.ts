3e8647679aca3feebff163bd59666811ed387953
guilherme <guile.hm@hotmail.com>
2022-01-17 20:46:31 -0300

    create dead sprite

--- animated/src/services/game-service.ts
+++ animated/src/services/game-service.ts
@@ -1,4 +1,4 @@
-import { Player } from "../types/rogue-types"
+import { Player, Sprite } from "../types/rogue-types"
 
 interface AnimationTime {
   [key: string]: [number, boolean]
@@ -7,6 +7,30 @@ const lastAnimationMap: AnimationTime = {}
 
 
 export const handleAnimations = (player: Player, now: number) => {
+  const deadSprite: Sprite = {
+    name: "Ghost",
+    tileSet: "sprites",
+    spriteX: 37,
+    spriteY: 56,
+    spriteWidth: 6,
+    spriteHeight: 7,
+    hp: 0,
+    xOffset: 1,
+    yOffset: 1,
+    animationPeriod: 0,
+    damage: 0,
+    attackRange: 0,
+    animation: {
+      spriteX: 0,
+      spriteY: 0,
+      spriteWidth: 0,
+      spriteHeight: 0,
+      xOffset: 0,
+      yOffset: 0,
+    }
+  }
+  if (player.dead) return deadSprite
+
   // TODO: improve performance
   let sprite
   let animationData = lastAnimationMap[player.id.toString()]

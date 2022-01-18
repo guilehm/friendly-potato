3628167756f2334da45b486646c4ccbc13bc7d45
guilherme <guile.hm@hotmail.com>
2022-01-18 00:22:17 -0300

    remove comment

--- animated/src/components/RogueLike/RogueLike.styles.ts
+++ animated/src/components/RogueLike/RogueLike.styles.ts
@@ -11,7 +11,6 @@ export const Container = styled.div`
 export const Canvas = styled.canvas`
   border: 5px solid black;
   box-shadow: 0px 0px 20px -3px #000000;
-  /* margin: auto; */
   width: 100%;
   max-width: 720px;
   image-rendering: optimizeSpeed; /* STOP SMOOTHING, GIVE ME SPEED  */
